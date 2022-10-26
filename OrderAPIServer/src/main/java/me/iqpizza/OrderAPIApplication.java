c386dc06a3c887c703f5196396a7f0ed25b709a4
iqpizza6349 <iqpizza6349@gmail.com>
2022-10-26 19:56:14 +0900

    :pushpin: :: Fix multi-module dependencies

--- OrderAPIServer/src/main/java/me/iqpizza/OrderAPIApplication.java
+++ OrderAPIServer/src/main/java/me/iqpizza/OrderAPIApplication.java
@@ -1,16 +1,12 @@
 package me.iqpizza;
 
 import me.iqpizza.config.jwt.JwtProperties;
-import me.iqpizza.config.jwt.JwtProvider;
-import me.iqpizza.config.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackageClasses = JwtProperties.class)
-@ComponentScan(basePackageClasses = {SecurityConfig.class, JwtProvider.class})
 public class OrderAPIApplication {
 
     public static void main(String[] args) {
